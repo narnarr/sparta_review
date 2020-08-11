796068d026bb61944a67bc03b3fee5e6312693ca
narnarr <narnarr@github.com>
2020-08-11 19:56:05 +0900

    Update 07_movie_scraping.py
    feat: choose date

--- 07_movie_scraping.py
+++ 07_movie_scraping.py
@@ -2,7 +2,12 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200716', headers=headers)
+url = 'https://movie.naver.com/movie/sdb/rank/rmovie.nhn'
+params = {
+    'sel': 'pnt',
+    'date': '20180327'
+}
+data = requests.get(url, headers=headers, params=params)
 soup = BeautifulSoup(data.text, 'html.parser')
 
 movies = soup.select('#old_content > table > tbody > tr')
